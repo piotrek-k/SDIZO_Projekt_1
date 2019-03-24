6bfc18a210f97ea55bcea8c553094dfc660eee74
Piotrek <kpiotrek3@gmail.com>
2019-03-24 12:23:39 +0100

    Organizing heap class

--- AppLibrary/Heap.h
+++ AppLibrary/Heap.h
@@ -5,7 +5,9 @@ class Heap
 {
 	int *tab;
 	int count; //zadeklarowana w pliku iloœæ elementów w tablicy
+
 	bool searchFromIndex(int val, int index); //funkcja pomocnicza do findValue
+
 public:
 	void loadFromFile(std::string FileName);
 
@@ -13,15 +15,17 @@ public:
 	int getLeftChildIndex(int index);
 	int getRightChildIndex(int index);
 
-	void populateFromArray(int * arr, int size);
 	void buildHeap();
 	void heapify(int index);
 
-	bool findValue(int val);
+	void populateFromArray(int * arr, int size);
 	void addElement(int key);
 	void removeElementAtIndex(int index);
-	std::string convertToString();
 	void generateRandom(int size);
+
+	bool findValue(int val);
+	
+	std::string convertToString();
 	int* toArray();
 	void display(ostream & stream);
 
